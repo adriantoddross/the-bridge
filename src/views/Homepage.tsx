2867e518c5b5fbe299eb466a972fb0e5a183f6a1
Adrian Ross <adriantoddross@gmail.com>
2020-09-10 15:21:26 -0400

    Format links

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -3,36 +3,56 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
+  link: {
     marginRight: theme.spacing(2),
-  },
-  title: {
     flexGrow: 1,
   },
 }));
 
 function Homepage() {
   const classes = useStyles();
+  const preventDefault = (event: React.MouseEvent) => event.preventDefault();
   return (
     <>
       <header>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="body1" className={classes.title}>
-              Home
-            </Typography>
+            <Typography className={classes.root}>
+              <Link
+                href="#"
+                onClick={preventDefault}
+                color="inherit"
+                variant="body1"
+                className={classes.link}
+              >
+                Home
+              </Link>
 
-            <Typography variant="body1" className={classes.title}>
-              About
-            </Typography>
+              <Link
+                href="#"
+                onClick={preventDefault}
+                color="inherit"
+                variant="body1"
+                className={classes.link}
+              >
+                About
+              </Link>
 
-            <Typography variant="body1" className={classes.title}>
-              Contact
+              <Link
+                href="#"
+                onClick={preventDefault}
+                color="inherit"
+                variant="body1"
+                className={classes.link}
+              >
+                Contact
+              </Link>
             </Typography>
           </Toolbar>
         </AppBar>
