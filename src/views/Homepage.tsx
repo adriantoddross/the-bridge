0f0afe889cf629aaff7040653a733793eb24d53a
Adrian Ross <adriantoddross@gmail.com>
2020-09-10 11:16:10 -0400

    Stub homepage view

---
+++ src/views/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <>
+      <header>Header</header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </>
+  );
+}
+
+export default Homepage;
