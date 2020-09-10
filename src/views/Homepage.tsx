a68246d3f29c8abf9ad73c1eaabe67fe25dd9927
Adrian Ross <adriantoddross@gmail.com>
2020-09-10 14:28:40 -0400

    Stub navbar

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Homepage() {
+  const classes = useStyles();
   return (
     <>
-      <header>Header</header>
+      <header>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="body1" className={classes.title}>
+              Home
+            </Typography>
+
+            <Typography variant="body1" className={classes.title}>
+              About
+            </Typography>
+
+            <Typography variant="body1" className={classes.title}>
+              Contact
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </header>
       <main>Main</main>
       <footer>Footer</footer>
     </>
