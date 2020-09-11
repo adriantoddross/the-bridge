e86e5909bfdf3fcbfac391a2c340ab2318ee8231
Adrian Ross <adriantoddross@gmail.com>
2020-09-11 11:30:24 -0400

    Stub hero image

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -1,16 +1,47 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
 import Navbar from "../components/Navbar";
 
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  heroImage: {},
+  heroText: {},
+  heroLink: {},
+}));
+
 function Homepage() {
+  const classes = useStyles();
+  const preventDefault = (event: React.MouseEvent) => event.preventDefault();
+
   return (
     <>
       <Navbar />
+
       <main>
-        <div>
-          <section>Hello world!</section>
+        <div className={[classes.heroImage, classes.root].join(" ")}>
+          <section className={classes.heroText}>
+            <Typography variant="h4" component="h1">
+              Hello, world!
+            </Typography>
+
+            <Typography variant="body1">Description</Typography>
+
+            <Link
+              href="#"
+              onClick={preventDefault}
+              color="inherit"
+              variant="body1"
+              className={classes.heroLink}
+            >
+              Learn more
+            </Link>
+          </section>
         </div>
       </main>
+
       <footer>Footer</footer>
     </>
   );
