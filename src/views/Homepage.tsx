7628d72438f1e916d21a02abbbe9a6773391b14c
Adrian Ross <adriantoddross@gmail.com>
2020-09-12 14:54:32 -0400

    Import search bar

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Search from "../components/Search";
 
 function Homepage() {
   return (
@@ -9,6 +10,7 @@ function Homepage() {
       <Navbar />
       <main>
         <Hero />
+        <Search />
       </main>
 
       <footer>Footer</footer>
