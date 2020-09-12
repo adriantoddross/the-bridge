5692625be8bbd5e6714faa96c781006c62169e52
Adrian Ross <adriantoddross@gmail.com>
2020-09-12 19:14:56 -0400

    Mock footer

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,10 +16,25 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
     },
   },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+
+    "& > *": {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+    },
+  },
 }));
 
 function Subscribe() {
   const classes = useStyles();
+  const [email, setEmail] = React.useState("");
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setEmail(event.target.value as string);
+  };
+  const preventDefault = (event: React.MouseEvent) => event.preventDefault();
+
   return (
     <Paper
       className={[classes.root, classes.paper].join(" ")}
@@ -30,6 +47,30 @@ function Subscribe() {
       <Typography variant="body2">
         Get notified of new organizations and other updates.
       </Typography>
+      <form
+        id="subscribe"
+        noValidate
+        autoComplete="off"
+        className={classes.form}
+      >
+        <TextField
+          id="email"
+          type="email"
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={handleChange}
+        />
+        <Button
+          variant="contained"
+          type="submit"
+          form="subscribe"
+          value={email}
+          onClick={preventDefault}
+        >
+          Subscribe
+        </Button>
+      </form>
     </Paper>
   );
 }
