743dd8fcc165f1a7ea282866268aa8bfcb483f6f
Adrian Ross <adriantoddross@gmail.com>
2020-09-13 11:04:14 -0400

    Stub theme edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Homepage from "./views/Homepage";
 import "./App.css";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Lato", "Garamond", "sans-serif"].join(","),
+  },
+  palette: {
+    primary: {
+      light: "#E2F4EC",
+      main: "#10844E",
+      dark: "#004B28",
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Homepage />
-    </>
+    </ThemeProvider>
   );
 }
 
