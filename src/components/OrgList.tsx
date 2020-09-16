75d88b2d3f90e46abf06221f7234145391c40b41
Adrian Ross <adriantoddross@gmail.com>
2020-09-15 20:25:27 -0400

    Successfuly filter search results

--- src/components/OrgList.tsx
+++ src/components/OrgList.tsx
@@ -4,11 +4,34 @@ import Search from "./Search";
 import OrgTable from "./OrgTable";
 
 function OrgList(props: any) {
-  const [orgs] = React.useState(props.orgs || []);
+  const [orgs, setOrgs] = React.useState(props.data.orgs || []);
+
+  const checkName = (name: String, query: String) => {
+    const pattern = query
+      .split("")
+      .map((x) => {
+        return `(?=.*${x})`;
+      })
+      .join("");
+
+    const regex = new RegExp(`${pattern}`, "g");
+    return name.match(regex);
+  };
+
+  const searchByQuery = (userInput: String) => {
+    const searchTerm = userInput.toLowerCase();
+
+    const filteredList = props.data.orgs.filter((org: any) => {
+      const name = org.name.toLowerCase();
+      return name.includes(searchTerm) || checkName(name, searchTerm);
+    });
+
+    // Update state in OrgTable!
+  };
 
   return (
     <>
-      <Search />
+      <Search searchByQuery={searchByQuery} />
       <OrgTable tableData={orgs} />
     </>
   );
