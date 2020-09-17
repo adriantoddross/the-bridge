3ba360c6b7faac37af55cadd105bd9dd038372cd
Adrian Ross <adriantoddross@gmail.com>
2020-09-17 15:22:30 -0400

    Update import

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import FeaturedOrgList from "../components/FeaturedOrgList";
-import OrgList from "../components/OrgList";
+import OrgList from "../components/OrgList/OrgList";
 import Subscribe from "../components/Subscribe";
 
 import mockData from "../mockData";
