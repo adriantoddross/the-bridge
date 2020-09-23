f5292ab6a2a0db13f5c2ce5a30290f2e40dc8861
Adrian Ross <adriantoddross@gmail.com>
2020-09-22 21:26:34 -0400

    Init react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Homepage from "./views/Homepage";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Homepage />
-    </>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <>
+            <Homepage />
+          </>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
