a54b8220213d0c3d3209a8b78411aa8593ead2fe
Adrian Ross <adriantoddross@gmail.com>
2020-09-24 10:35:09 -0400

    Finish 404 page

--- src/views/404Page.tsx
+++ src/views/404Page.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { Link as RouterLink } from "react-router-dom";
+
+import Navbar from "../components/Navbar";
+import Subscribe from "../components/Subscribe";
+import Hero from "../components/Hero";
 
 export default function PageNotFound() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <Navbar />
+      <main>
+        <Hero
+          cloudinaryImageTitle="pexels-ketut-subiyanto-4560092"
+          objectPosition="middle"
+          title="404"
+          description="Oops! The page you’re looking for doesn’t exist."
+        >
+          <Typography variant="body2">
+            The page may have been moved or typed incorrectly.
+          </Typography>
+          <Button
+            to="/"
+            color="inherit"
+            variant="contained"
+            component={RouterLink}
+          >
+            Go back home
+          </Button>
+        </Hero>
+      </main>
+      <footer>
+        <Subscribe />
+      </footer>
     </>
   );
 }
