76e0f0c81ff35fbd01afacde788bb6b6b5c80da7
Adrian Ross <adriantoddross@gmail.com>
2020-09-25 16:52:10 -0400

    Stub org page component

---
+++ src/views/OrgPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import Typography from "@material-ui/core/Typography";
+
+import Navbar from "../components/Navbar";
+import Subscribe from "../components/Subscribe";
+// import ContentBlock from "../components/ContentBlock";
+
+export default function OrgPage(props) {
+  return (
+    <>
+    <Navbar/>
+    <main>
+      <h1>Hello world!</h1>
+    </main>
+    <Subscribe/>
+    </>
+  );
+}
