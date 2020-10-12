a00d9f7a53719b2540d724661b36eed3ec0fdf38
Adrian Ross <adriantoddross@gmail.com>
2020-10-12 18:00:41 -0400

    Render almost all org info

--- src/views/OrgPage.tsx
+++ src/views/OrgPage.tsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
 import Navbar from "../components/Navbar";
 import Subscribe from "../components/Subscribe";
-// import ContentBlock from "../components/ContentBlock";
+import ContentBlock from "../components/ContentBlock";
 import mockData from "../mockData";
 
 export default function OrgPage(props) {
   let { name } = useParams();
-  const [orgData, setOrgData] = useState({});
+  const [orgData, setOrgData] = useState<any>({});
 
   useEffect(() => {
     const org = mockData.orgs.find(
@@ -23,7 +23,25 @@ export default function OrgPage(props) {
     <>
       <Navbar />
       <main>
-        <h1>Hello world!</h1>
+        <ContentBlock
+          cloudinaryImageTitle={orgData.cloudinaryImageTitle}
+          title={orgData.name}
+          objectPosition="middle"
+        >
+          <Typography variant="body2">{orgData.category}</Typography>
+          <div>
+            <Typography variant="body1">
+              <b>{orgData.needs?.description}</b>
+            </Typography>
+          </div>
+          <div>
+            {orgData.impact?.map((impact) => (
+              <Typography variant="h6" component="p">
+                {impact}
+              </Typography>
+            ))}
+          </div>
+        </ContentBlock>
       </main>
       <Subscribe />
     </>
