54f9f5bc3f83918be096ab88638c8a986c61f07c
Adrian Ross <adriantoddross@gmail.com>
2020-11-03 00:44:29 -0500

    Stub contact form

--- src/views/ContactPage.tsx
+++ src/views/ContactPage.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core";
 
 import Navbar from "../components/Navbar";
 import Subscribe from "../components/Subscribe";
 import ContentBlock from "../components/ContentBlock";
 
 function ContactPage() {
+  const useStyles = makeStyles((theme) => ({
+    form: {
+      display: "flex",
+      flexDirection: "column",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <>
       <Navbar />
-
       <main>
         <ContentBlock
           cloudinaryImageTitle=""
@@ -21,6 +32,28 @@ function ContactPage() {
               Have a question? Want to partner with us? Send us a message?
             </Typography>
           </section>
+          <div>
+            <form noValidate autoComplete="off" className={classes.form}>
+              <TextField id="sender-name" label="Name" variant="outlined" />
+              <TextField id="sender-email" label="Email" variant="outlined" />
+              <TextField
+                id="sender-message"
+                label="Message"
+                variant="outlined"
+                multiline={true}
+                rows={6}
+                rowsMax={6}
+              />
+              <Button
+                type="submit"
+                value="submit"
+                color="inherit"
+                variant="contained"
+              >
+                Send message
+              </Button>
+            </form>
+          </div>
         </ContentBlock>
       </main>
 
