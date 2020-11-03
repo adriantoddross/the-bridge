30747d8a8d8095b3132690c7baf7eea05348be6d
Adrian Ross <adriantoddross@gmail.com>
2020-11-02 22:52:38 -0500

    Initialize react ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import ReactGA from "react-ga";
+
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -10,6 +12,8 @@ if (process.env.NODE_ENV !== "production") {
   var axe = require("react-axe");
   axe(React, ReactDOM, 1000);
 }
+
+ReactGA.initialize("G-RC4G6Z0N3V");
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
