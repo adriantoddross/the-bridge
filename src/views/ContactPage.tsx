ddd33b18caad836dbfaf44494d08226bf3a8a165
Adrian Ross <adriantoddross@gmail.com>
2020-11-04 22:19:55 -0500

    Import contact form

--- src/views/ContactPage.tsx
+++ src/views/ContactPage.tsx
@@ -1,39 +1,12 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField/TextField";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core";
 
 import Navbar from "../components/Navbar";
 import Subscribe from "../components/Subscribe";
 import ContentBlock from "../components/ContentBlock";
+import ContactForm from "../components/ContactForm";
 
 function ContactPage() {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
-
-  const useStyles = makeStyles((theme) => ({
-    form: {
-      display: "flex",
-      flexDirection: "column",
-    },
-  }));
-
-  const classes = useStyles();
-
-  const handleName = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setName(event.target.value as string);
-  };
-
-  const handleEmail = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setEmail(event.target.value as string);
-  };
-
-  const handleMessage = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setMessage(event.target.value as string);
-  };
-
   return (
     <>
       <Navbar />
@@ -49,40 +22,7 @@ function ContactPage() {
             </Typography>
           </section>
           <div>
-            <form noValidate autoComplete="off" className={classes.form}>
-              <TextField
-                id="sender-name"
-                label="Name"
-                variant="outlined"
-                value={name}
-                onChange={handleName}
-              />
-              <TextField
-                id="sender-email"
-                label="Email"
-                variant="outlined"
-                value={email}
-                onChange={handleEmail}
-              />
-              <TextField
-                id="sender-message"
-                label="Message"
-                variant="outlined"
-                multiline={true}
-                rows={6}
-                rowsMax={6}
-                value={message}
-                onChange={handleMessage}
-              />
-              <Button
-                type="submit"
-                value="submit"
-                color="inherit"
-                variant="contained"
-              >
-                Send message
-              </Button>
-            </form>
+            <ContactForm />
           </div>
         </ContentBlock>
       </main>
