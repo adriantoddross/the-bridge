4a33b34d8d4df8c04a13b685128fa6f5e1b52a43
Adrian Ross <adriantoddross@gmail.com>
2020-11-04 23:35:45 -0500

    Stub handleChange and test it on the name input

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -7,6 +7,11 @@ function ContactForm() {
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
+  const [errors, setErrors] = React.useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
   const useStyles = makeStyles((theme) => ({
     form: {
@@ -29,6 +34,39 @@ function ContactForm() {
     setMessage(event.target.value as string);
   };
 
+  const handleChange = (
+    event: React.ChangeEvent<{ name: string; value: string }>
+  ) => {
+    event.preventDefault();
+
+    const { name, value } = event.target;
+    console.log(event.target);
+    console.log("name:", name, "value:", value);
+    let formErrors = { ...errors };
+
+    switch (name) {
+      case "name":
+        setName(value);
+        formErrors.name = value.length ? "" : "Please provide your name.";
+        break;
+
+      case "email":
+        formErrors.email = value.length
+          ? ""
+          : "Please provide a valid e-mail address.";
+        break;
+
+      case "message":
+        formErrors.name = value.length ? "" : "Please provide a message.";
+        break;
+
+      default:
+        break;
+    }
+
+    setErrors(formErrors);
+  };
+
   const handleFormSubmission = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
@@ -40,15 +78,17 @@ function ContactForm() {
     <>
       <form noValidate autoComplete="on" className={classes.form}>
         <TextField
+          name="name"
           type="text"
           id="sender-name"
           label="Name"
           variant="outlined"
           value={name}
-          onChange={handleName}
+          onChange={handleChange}
           required
         />
         <TextField
+          name="email"
           type="email"
           id="sender-email"
           label="Email"
@@ -58,6 +98,7 @@ function ContactForm() {
           required
         />
         <TextField
+          name="message"
           type="text"
           id="sender-message"
           label="Message"
