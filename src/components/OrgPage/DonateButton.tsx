839866179014a746a741d8bb2a9fc98b51cef158
Adrian Ross <adriantoddross@gmail.com>
2021-05-20 08:58:53 -0400

    Extract donate button from OrgPage

--- src/components/OrgPage/DonateButton.tsx
+++ src/components/OrgPage/DonateButton.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import ReactGA from "react-ga";
+import { Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-function donateButton() {
-  return <button>Donate on Website</button>;
+const useStyles = makeStyles(() => ({
+  link: {
+    fontStyle: "italic",
+    textDecoration: "underline",
+  },
+}));
+
+export default function donateButton(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Link
+        eventLabel={`Clicked donate link for: ${props.orgData.name}`}
+        to={
+          props.orgData?.links?.learnMore ||
+          props.orgData?.links?.donate ||
+          props.orgData?.links?.website
+        }
+        target="_blank"
+        rel="noopener"
+        variant="body1"
+        component={ReactGA.OutboundLink}
+        className={classes.link}
+      >
+        {props.orgData?.links?.title || "Donate on Website"}
+      </Link>
+    </div>
+  );
 }
-
-export default donateButton;
