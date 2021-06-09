29c9feed4a4d599fac648674903f5b62a4111a72
Adrian Ross <adriantoddross@gmail.com>
2021-06-09 08:50:51 -0400

    Remove unused import

--- src/components/OrgPage/DonateButton.tsx
+++ src/components/OrgPage/DonateButton.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactGA from "react-ga";
 import Button from "@material-ui/core/Button";
-import { Link } from "@material-ui/core";
 
 export default function DonateButton(props) {
   return (
