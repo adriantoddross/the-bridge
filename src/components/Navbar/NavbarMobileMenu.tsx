fb42cbda41a270b11d2a16cd6d026c9d9ef5f3e3
Adrian Ross <adriantoddross@gmail.com>
2021-06-22 08:23:46 -0400

    Fully extract mobile menu

--- src/components/Navbar/NavbarMobileMenu.tsx
+++ src/components/Navbar/NavbarMobileMenu.tsx
@@ -1,41 +1,58 @@
 import React from "react";
 import { Menu, MenuItem, Link } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function MobileNavLinks() {
   const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
 
   const handleClose = () => {
     setAnchorEl(null);
   };
 
   return (
-    <Menu
-      id="simple-menu"
-      anchorEl={anchorEl}
-      keepMounted
-      open={Boolean(anchorEl)}
-      onClose={handleClose}
-    >
-      <MenuItem onClick={handleClose}>
-        {" "}
-        <Link component={RouterLink} to="/">
-          Home
-        </Link>
-      </MenuItem>
+    <>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        color="inherit"
+      >
+        <MenuIcon />
+        <span className="visually-hidden">Open navigation menu</span>
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          {" "}
+          <Link component={RouterLink} to="/">
+            Home
+          </Link>
+        </MenuItem>
 
-      <MenuItem onClick={handleClose}>
-        <Link component={RouterLink} to="/about">
-          About
-        </Link>
-      </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link component={RouterLink} to="/about">
+            About
+          </Link>
+        </MenuItem>
 
-      <MenuItem onClick={handleClose}>
-        <Link component={RouterLink} to="/contact">
-          Contact
-        </Link>
-      </MenuItem>
-    </Menu>
+        <MenuItem onClick={handleClose}>
+          <Link component={RouterLink} to="/contact">
+            Contact
+          </Link>
+        </MenuItem>
+      </Menu>
+    </>
   );
 }
 
